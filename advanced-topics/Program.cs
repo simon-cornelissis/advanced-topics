2acebd3c73ff6219d4722d12929af637ecad4673
Simon Cornelissis <simon.cornelissis@student.howest.be>
2026-02-23 11:00:06 +0100

    excersise 1b

--- advanced-topics/Program.cs
+++ advanced-topics/Program.cs
@@ -16,3 +16,19 @@
 Fibonacci(10)
         .ToList()
         .ForEach(number => Console.WriteLine(number));
+
+static string Reverse(string text)
+{
+    if (string.IsNullOrEmpty(text))
+        return text;
+    return text[^1] + Reverse(text[..^1]);
+}
+
+Console.WriteLine("Please enter a text: ");
+string myText = Console.ReadLine().Trim().ToLower();
+
+string myReversedText = Reverse(myText);
+
+Console.WriteLine(myReversedText);
+	
+Console.WriteLine($"The text {myText} is { (myText == myReversedText ? "a" : "not a")} palindrome"); 
