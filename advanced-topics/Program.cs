8b61144bde679ee82239f02f13cb911bfc45d016
Simon Cornelissis <simon.cornelissis@student.howest.be>
2026-02-23 10:40:48 +0100

    excersise 1a

--- advanced-topics/Program.cs
+++ advanced-topics/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿static IEnumerable<int> Fibonacci(int number) 
+{
+    int a = 0, b = 1;
+    yield return a;
+    yield return b;
+    for (int i = 2; i <= number; i++)
+    {
+        int next = a + b;
+        yield return next;
+
+        a = b;
+        b = next;
+    }
+}
+
+Fibonacci(10)
+        .ToList()
+        .ForEach(number => Console.WriteLine(number));
